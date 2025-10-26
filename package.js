32883b31bc38470fa23e7b03dea675590b89d5e4
JackHONGhy <153146840+JackHONGhy@users.noreply.github.com>
2025-10-26 12:31:47 +0800

    Initialize package.json for stress-tester project

---
+++ package.js
@@ -0,0 +1,10 @@
+{
+  "name": "stress-tester",
+  "version": "1.0.0",
+  "main": "master-server.js",
+  "dependencies": {
+    "ws": "^8.18.0",
+    "got": "^13.0.0",
+    "cluster": "^0.7.7"
+  }
+}
